7b11faced53bc57a1fe0daac2165817bab85ae55
bienpacman <biendz2000@gmail.com>
2022-05-17 21:01:25 +0700

    remake

---
+++ src/com/company/MyListTest.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class MyListTest {
+
+    public static void main(String[] args) {
+	MyList<Integer> integerMyList = new MyList<Integer>();
+    integerMyList.add(1);
+    integerMyList.add(2);
+    integerMyList.add(3);
+    integerMyList.add(3);
+    integerMyList.add(4);
+
+        System.out.println("element 4 : " + integerMyList.get(4));
+        System.out.println("element 1 : " + integerMyList.get(1));
+        System.out.println("element 2 : " + integerMyList.get(2));
+        System.out.println("element 3 : " + integerMyList.get(3));
+
+
+
+        integerMyList.get(-1);
+        System.out.println("element -1 : " + integerMyList.get(-1));
+    }
+}
